34dcaaa5b02b5afc4dc4c25b1406d9d733dd7997
Anders Hellerup Madsen <ahem@github.com>
2023-02-27 10:55:55 +0100

    wat

--- src/cards.rs
+++ src/cards.rs
@@ -4,5 +4,17 @@
 // TODO: Pumpkin Patch Party: "In this round, every orange chip is moved 1 extra space forward" (blue)
 // TODO: You Only Get to Choose One: "Choose: Take 1 black chip OR take any one 2-chip OR 3 rubies" (purple)
 // TODO: Choose wisely: "Choose: move droplet 2 spaces forward, OR take 1 purple chip" (purple)
-// TODO: An Opportunistic Moment: "Draw 4 chips from your bag. You may trade 1 of them for a chip of the same color with the next higher value. If you can't make a trade, take 1 green chip. Put all the chips back in the bag" (purple).
+// TODO: An Opportunistic Moment: "Draw 4 chips from your bag. You may trade 1 of them for a chip of the same color with the next higher value. If you can't make a trade, take 1 green chip. Put all the chips back in the bag" (purple)
+// TODO: Less is More: "All players draw 5 chips. The player(s) with the lowest sum take(s) 1 blue 2-chip. All other players receive 1 ruby. Put all the chips back in the bag." (purple)
+// TODO: It's Shining Extra Bright: "If you reach a scoring space with a ruby, you get an extra ruby." (blue)
+// TODO: Strong Ingredient: "Beginning with the start player, if you stopped without an explosion, draw up to 5 chips from your bag. You may place 1 of them in your pot." (blue)
+// TODO: Lucky Devil: "If you reach a scoring space with a ruby this round, you get an extra 2 victory points, even if your pot has exploded." (blue)
+// TODO: The Pot is Full: "Any player who gets to roll the die this turn round rolls twice" (blue)
+// TODO: Living in Luxury: "The threshold for white chips is raised in this round from 7 to 9" (blue)
+// TODO: The Pot is Filling Up: "Move your droplet 1 space forward" (purple)
+// TODO: Beginner's Bonus: "The player(s) with the fewest victory points receive(s) 1 green 1-chip" (purple)
+// TODO: Rats are your Friends: "Choose: Take any one 4-chip OR 1 victory point for each rat tail you get this turn." (purple)"
+// TODO: Just in Time: "Choose: Take 4 vicotry points OR remove 1 white 1-chip from your bag" (purple)
+// TODO: Charity: "The player(s) with the fewest rubies receive(s) 1 ruby." (purple)
+// TODO: Well Strirred: "In this round, you may put the first white chip you draw back into the bag." (blue)
 // fmt: on
